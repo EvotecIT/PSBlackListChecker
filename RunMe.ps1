471b6979f06c22e62cc9608b27f8e8fcb05e7dc5
PrzemyslawKlys <pklys@evotec.pl>
2018-04-29 13:31:20 +0200

    Added few examples

--- RunMe.ps1
+++ RunMe.ps1
@@ -1,3 +1,7 @@
-Import-Module .\PSBlackListChecker.psm1
+Import-Module PSBlackListChecker.psm1 -Force
 
-Search-BlackList -IP '89.25.253.1' | Format-Table
+Search-BlackList -IP '89.25.253.18' -ReturnAll | Format-Table -AutoSize
+
+Search-BlackList -IP '89.25.253.1' -ReturnAll -SortBy IsListed -SortDescending $true
+Search-BlackList -IP '89.25.253.1', '89.25.253.2' -ReturnAll -SortBy Ip | Format-Table -AutoSize
+Search-BlackList -IP '89.25.253.1', '89.25.253.2' -ReturnAll -SortBy BlackList | Format-Table -AutoSize
