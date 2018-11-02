e648e3fc78cd457e29b67d3e36df214d627582b6
PrzemyslawKlys <pklys@evotec.pl>
2018-11-02 21:40:22 +0100

    Update to tests

--- PSBlackListChecker.Tests.ps1
+++ PSBlackListChecker.Tests.ps1
@@ -11,16 +11,14 @@ if ($null -eq $Pester -or ($Pester[0].Version.Major -le 4 -and $Pester[0].Versio
     Write-Warning "$ModuleName - Downloading Pester from PSGallery"
     Install-Module -Name Pester -Repository PSGallery -Force -SkipPublisherCheck -Scope CurrentUser
 }
-$Module = Get-Module -ListAvailable PSSharedGoods
-if ($null -eq $Module) {
-    Write-Warning "$ModuleName - Downloading PSSharedGoods from PSGallery"
-    Install-Module -Name PSSharedGoods -Repository PSGallery -Force -Scope CurrentUser
-}
 
-$Module = Get-Module -ListAvailable PSWriteColor
-if ($null -eq $Module) {
-    Write-Warning "$ModuleName - Downloading PSWriteColor from PSGallery"
-    Install-Module -Name PSWriteColor -Repository PSGallery -Force -Scope CurrentUser
+$RequiredModules = (Get-Content -Raw $PSScriptRoot\*.psd1)  | Invoke-Expression | ForEach-Object RequiredModules
+foreach ($Module in $RequiredModules) {
+    $ModuleFound = Get-Module -ListAvailable $Module
+    if ($null -eq $ModuleFound) {
+        Write-Warning "$ModuleName - Downloading $Module from PSGallery"
+        Install-Module -Name $Module -Repository PSGallery -Force -Scope CurrentUser
+    }
 }
 
 $result = Invoke-Pester -Script @{ Path = "$($PSScriptRoot)\Tests"; Parameters = @{ TeamsID = $TeamsID; SlackID = $SlackID } } -EnableExit
