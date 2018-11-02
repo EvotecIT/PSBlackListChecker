4adda05f9cedf8c7dc094c05f96809979b37f614
PrzemyslawKlys <pklys@evotec.pl>
2018-11-02 20:04:32 +0100

    Update

--- Tests/Search-Blacklists.Tests.ps1
+++ Tests/Search-Blacklists.Tests.ps1
@@ -10,32 +10,32 @@ $IP = '89.74.48.96'
 Describe 'Search-Blacklists - Should test IP for blacklists' {
     It 'Given 1 IP - No Workflow or RunSpaces using [Net.DNS]- Should return at least 2 listed blacklists' {
         $BlackList = Search-Blacklist -IP $IP -RunType NoWorkflowAndRunSpaceNetDNS
-        $BlackList.Count | Should -BeGreaterThan 2
+        $BlackList.Count | Should -BeGreaterThan 1
         $BlackList.IsListed | Should -Contain $True
     }
     It 'Given 1 IP - No Workflow or RunSpaces using [Resolve-DnsName] - Should return at least 2 listed blacklists' {
         $BlackList = Search-Blacklist -IP $IP -RunType NoWorkflowAndRunSpaceResolveDNS
-        $BlackList.Count | Should -BeGreaterThan 2
+        $BlackList.Count | Should -BeGreaterThan 1
         $BlackList.IsListed | Should -Contain $True
     }
     It 'Given 1 IP - RunSpaces using [Net.DNS] - Should return at least 2 listed blacklists' {
         $BlackList = Search-Blacklist -IP $IP -RunType RunSpaceWithNetDNS
-        $BlackList.Count | Should -BeGreaterThan 2
+        $BlackList.Count | Should -BeGreaterThan 1
         $BlackList.IsListed | Should -Contain $True
     }
     It 'Given 1 IP - RunSpaces using [Resolve-DnsName] - Should return at least 2 listed blacklists' {
         $BlackList = Search-Blacklist -IP $IP -RunType RunSpaceWithResolveDNS
-        $BlackList.Count | Should -BeGreaterThan 2
+        $BlackList.Count | Should -BeGreaterThan 1
         $BlackList.IsListed | Should -Contain $True
     }
     It 'Given 1 IP - Workflow using [Resolve-DnsName] - Should return at least 2 listed blacklists' {
         $BlackList = Search-Blacklist -IP $IP -RunType WorkflowResolveDNS
-        $BlackList.Count | Should -BeGreaterThan 2
+        $BlackList.Count | Should -BeGreaterThan 1
         $BlackList.IsListed | Should -Contain $True
     }
     It 'Given 1 IP - Workflow using [Net.DNS] - Should return at least 2 listed blacklists' {
         $BlackList = Search-Blacklist -IP $IP -RunType WorkflowWithNetDNS
-        $BlackList.Count | Should -BeGreaterThan 2
+        $BlackList.Count | Should -BeGreaterThan 1
         $BlackList.IsListed | Should -Contain $True
     }
 }
