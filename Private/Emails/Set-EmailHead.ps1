208652768a222761558327a9c292fe0cbd22f5c3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-05 15:24:01 +0200

    Moved functions from PSSharedGoods

---
+++ Private/Emails/Set-EmailHead.ps1
@@ -0,0 +1,78 @@
+function Set-EmailHead {
+    [cmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $FormattingOptions
+    )
+    $head = @"
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <style>
+    BODY {
+        background-color: white;
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
+
+    TABLE {
+        border-width: 1px;
+        border-style: solid;
+        border-color: black;
+        border-collapse: collapse;
+        font-family: $($FormattingOptions.FontTableDataFamily);
+        font-size: $($FormattingOptions.FontTableDataSize);
+    }
+
+    TH {
+        border-width: 1px;
+        padding: 3px;
+        border-style: solid;
+        border-color: black;
+        background-color: #00297A;
+        color: white;
+        font-family: $($FormattingOptions.FontTableHeadingFamily);
+        font-size: $($FormattingOptions.FontTableHeadingSize);
+    }
+    TR {
+        font-family: $($FormattingOptions.FontTableDataFamily);
+        font-size: $($FormattingOptions.FontTableDataSize);
+    }
+
+    UL {
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
+
+    LI {
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
+
+    TD {
+        border-width: 1px;
+        padding-right: 2px;
+        padding-left: 2px;
+        padding-top: 0px;
+        padding-bottom: 0px;
+        border-style: solid;
+        border-color: black;
+        background-color: white;
+        font-family: $($FormattingOptions.FontTableDataFamily);
+        font-size: $($FormattingOptions.FontTableDataSize);
+    }
+
+    H2 {
+        font-family: $($FormattingOptions.FontHeadingFamily);
+        font-size: $($FormattingOptions.FontHeadingSize);
+    }
+
+    P {
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
+</style>
+</head>
+"@
+    return $Head
+}
---
+++ Private/Emails/Set-EmailReportBranding.ps1
@@ -0,0 +1,26 @@
+function Set-EmailReportBranding {
+    param(
+        [alias('FormattingOptions')] $FormattingParameters
+    )
+    if ($FormattingParameters.CompanyBranding.Link) {
+        $Report = "<a style=`"text-decoration:none`" href=`"$($FormattingParameters.CompanyBranding.Link)`" class=`"clink logo-container`">"
+    } else {
+        $Report = ''
+    }
+    if ($FormattingParameters.CompanyBranding.Inline) {
+        $Report += "<img width=<fix> height=<fix> src=`"cid:logo`" border=`"0`" class=`"company-logo`" alt=`"company-logo`"></a>"
+    } else {
+        $Report += "<img width=<fix> height=<fix> src=`"$($FormattingParameters.CompanyBranding.Logo)`" border=`"0`" class=`"company-logo`" alt=`"company-logo`"></a>"
+    }
+    if ($FormattingParameters.CompanyBranding.Width -ne "") {
+        $Report = $Report -replace "width=<fix>", "width=$($FormattingParameters.CompanyBranding.Width)"
+    } else {
+        $Report = $Report -replace "width=<fix>", ""
+    }
+    if ($FormattingParameters.CompanyBranding.Height -ne "") {
+        $Report = $Report -replace "height=<fix>", "height=$($FormattingParameters.CompanyBranding.Height)"
+    } else {
+        $Report = $Report -replace "height=<fix>", ""
+    }
+    return $Report
+}
