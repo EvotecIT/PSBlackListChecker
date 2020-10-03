2b23b078499e102c43ed44c2075fae63aceb2bdc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-03 18:23:26 +0200

    Module

--- Examples/Search-BlackList-Manual.ps1
+++ Examples/Search-BlackList-Manual.ps1
@@ -1,4 +1,3 @@
-Import-Module ..\PSSharedGoods\PSSharedGoods.psd1 -Force
 Import-Module ..\PSBlackListChecker -Force
 
 $IP = '89.74.48.96'
@@ -6,6 +5,6 @@ $IP1 = '89.74.48.97'
 $MultipleIP = $IP, $IP1
 
 Write-Color "Test 1" -Color Red
-Search-BlackList -IP $IP -Verbose  | Format-Table -AutoSize
+Search-BlackList -IP $IP | Format-Table -AutoSize
 Write-Color "Test 2" -Color Red
-Search-BlackList -IP $IP -ReturnAll -Verbose | Format-Table -AutoSize
+Search-BlackList -IP $IP -ReturnAll | Format-Table -AutoSize
